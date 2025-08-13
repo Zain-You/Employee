911ebb4e03c667af3d95321d2586c68e0fe4ac0d
20047683 <20047683@qq.com>
2025-08-13 10:22:58 +0800

    Initial commit

---
+++ src/main/java/com/example/mytest/pojo/User.java
@@ -0,0 +1,60 @@
+package com.example.mytest.pojo;
+import jakarta.persistence.*;
+
+@Table(name = "tb_user")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Integer id;
+
+    @Column(name = "user_name")
+    private  String userName;
+
+    private  String password;
+
+    private  String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
